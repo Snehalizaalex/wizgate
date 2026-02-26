170baaa4051acce84dcc76003dd4608aa63b1299
sneha <snehalizaa@gmail.com>
2026-02-26 22:03:12 +0530

    Configure Vite for Multi-Page Build to fix Vercel 404 Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 
+import { resolve } from 'path'
+
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                contact: resolve(__dirname, 'contact.html'),
+                courses: resolve(__dirname, 'courses.html'),
+                test: resolve(__dirname, 'test.html')
+            }
+        }
+    },
     server: {
         port: 5173,
         strictPort: true,
